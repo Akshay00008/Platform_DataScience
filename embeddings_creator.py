73a390bfc05c9527520cc189dce7becec27517b3
Akshay00008 <Akshay.sharma@algo8.ai>
2025-06-24 21:03:22 +0530

    id mismatch

--- embeddings_creator.py
+++ embeddings_creator.py
@@ -394,7 +394,7 @@ def embeddings_from_website_content(json_data,  chatbot_id, version_id):
     faiss_index = FAISS.from_texts(text_chunks, embeddings, metadatas=chunk_metadata)
  
     # Save index and metadata
-    index_dir  = "C:\\Users\\hp\\Desktop\\Platform_16-05-2025\\faiss_indexes" #"/home/bramhesh_srivastav/Platform_DataScience/faiss_indexes"  # Directory where indexes will be saved
+    index_dir  = "/home/bramhesh_srivastav/Platform_DataScience/faiss_indexes"  # Directory where indexes will be saved
     os.makedirs(index_dir, exist_ok=True)
     # Generate a unique index filename based on chatbot_id and version_id
     index_filename = f"{chatbot_id}_{version_id}_faiss_index_website"
